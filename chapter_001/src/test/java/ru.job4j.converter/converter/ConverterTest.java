ca662ebb95796ee953ecef98860360b584edfb38
Mikhail Ford <mfordf150@mail.ru>
2019-10-03 17:45:29 +0300

    3.

---
+++ chapter_001/src/test/java/ru.job4j.converter/converter/ConverterTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.converter;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class ConverterTest {
+
+
+    @Test
+    public void rubleToEuro(){
+
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        Assert.assertEquals(expected,out);
+    }
+    @Test
+    public void rubleToDollar() {
+
+    }
+}
