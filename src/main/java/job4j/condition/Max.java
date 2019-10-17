0b7529408f6d0d030c833730a07b25c6f67b0fb0
Mikhail Ford <mfordf150@mail.ru>
2019-10-17 21:52:09 +0300

    максимум из 2

---
+++ src/main/java/job4j/condition/Max.java
@@ -0,0 +1,15 @@
+package job4j.condition;
+
+public class Max {
+
+
+
+    public static int max(int left, int right) {
+
+
+
+        int result = left;
+        return result < left ? result : right;
+    }
+
+}
