f34b23e83dfb4d935c7b0aec3f1fcfd50b236029
Mikhail Ford <mfordf150@mail.ru>
2019-10-24 17:53:23 +0300

    треугольник

--- src/main/java/job4j/condition/MultiMax.java
+++ src/main/java/job4j/condition/MultiMax.java
@@ -13,4 +13,5 @@ public class MultiMax {
         return result;
     }
 
+
 }
--- src/test/java/job4j/condition/MultiMaxTest.java
+++ src/test/java/job4j/condition/MultiMaxTest.java
@@ -2,7 +2,6 @@ package job4j.condition;
 
 import org.junit.Test;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class MultiMaxTest {
@@ -16,24 +15,24 @@ public class MultiMaxTest {
 
     @Test
     public void whenSecondMax2() {
-        MultiMax check = new MultiMax();
-        int result = check.max(1, 4, 2);
-        assertThat(result, is(4));
+        MultiMax check2 = new MultiMax();
+        int result2 = check2.max(1, 4, 2);
+        assertThat(result2, is(4));
 
     }
 
     @Test
     public void whenSecondMax3() {
-        MultiMax check = new MultiMax();
-        int result = check.max(1, 4, 2);
-        assertThat(result, is(4));
+        MultiMax check3 = new MultiMax();
+        int result3 = check3.max(1, 4, 2);
+        assertThat(result3, is(4));
 
     }
 
     @Test
     public void whenSecondMax4() {
-        MultiMax check = new MultiMax();
-        int result = check.max(1, 4, 2);
-        assertThat(result, is(4));
+        MultiMax check4 = new MultiMax();
+        int result3 = check4.max(1, 4, 2);
+        assertThat(result3, is(4));
     }
 }
