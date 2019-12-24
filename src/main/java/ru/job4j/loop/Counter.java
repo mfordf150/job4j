ace3078052735e34265e4f83a6165bde90a27db7
Mikhail Ford <mfordf150@mail.ru>
2019-12-24 23:21:26 +0300

    Подсчет суммы

---
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Counter {
+
+
+    public int add(int start, int finish) {
+
+        for (int add = 0; add < 30 ; add++) {
+
+        }
+        int sum = 0;
+        return sum;
+    }
+}
+
