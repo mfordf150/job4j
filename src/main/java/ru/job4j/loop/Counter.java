ba9f5ac274d752946fb418437fd34b429a2ae666
Mikhail Ford <mfordf150@mail.ru>
2020-01-17 19:00:39 +0300

    Подсчет суммы

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -3,20 +3,18 @@ package ru.job4j.loop;
 public class Counter {
 
 
-
-
     public int add(int start, int finish) {
 
-        int i;
+        int i, sum = 0;
         if (start % 2 == 0) {
             start++;
         }
         for (i = start; i <= finish; i += 2) {
-
+            sum += i;
         }
 
 
-        int sum = 0;
+        sum = 0;
         return sum;
     }
 }
--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -2,7 +2,6 @@ package ru.job4j.loop;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 import static sun.nio.cs.Surrogate.is;
 
 public class CounterTest {
@@ -10,8 +9,11 @@ public class CounterTest {
     public void whenSumEvenNumbersFromOneToTenThenThirty() {
         Counter counter = new Counter();
         int result = counter.add(2, 11);
-        assertEquals(result, is(30));
+        assertThat(result, is(30));
     
+    }
+
+    private void assertThat(int result, boolean b) {
     }
 
 
