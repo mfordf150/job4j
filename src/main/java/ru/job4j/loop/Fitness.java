52a6cd825e5928ef54a31fb7f12faa279e4c046c
Mikhail Ford <mfordf150@mail.ru>
2020-01-29 20:19:20 +0300

    Fitness

---
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+    if (ivan < nik) {
+
+    }
+    else {
+        ivan = 90;
+        while (ivan < 100) {
+           ivan *=3;
+        }
+    }
+
+
+        int month = 0;
+        return month;
+    }
+}
