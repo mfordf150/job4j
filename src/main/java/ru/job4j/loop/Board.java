7ac74ecfbbf5a98fb6b38c1a63aff3b952889266
Mikhail Ford <mfordf150@mail.ru>
2020-01-29 18:46:01 +0300

    Board

---
+++ src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,27 @@
+package ru.job4j.loop;
+
+public class Board {
+    public static void main(String[] args) {
+
+        Board board = new Board();
+
+        System.out.println(board.paint(5, 4));
+    }
+
+    StringBuilder vertical = new StringBuilder();
+
+    public String paint(int width, int height) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if ((i + j) % 2 == 0) {
+                    vertical.append(' ');
+                } else {
+                    vertical.append('x');
+                }
+            }
+            vertical.append('\n');
+        }
+
+        return vertical.toString();
+    }
+}
