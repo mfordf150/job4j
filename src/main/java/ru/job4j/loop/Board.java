7f80f5776d61d3a3e54a468fd792b6a92e773f6a
Mikhail Ford <mfordf150@mail.ru>
2020-01-30 08:04:02 +0300

    Fitness

--- src/main/java/ru/job4j/loop/Board.java
+++ src/main/java/ru/job4j/loop/Board.java
@@ -13,7 +13,7 @@ public class Board {
     public String paint(int width, int height) {
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                if ((i + j) % 2 == 0) {
+                if ((i + j) % 2 == 1) {
                     vertical.append(' ');
                 } else {
                     vertical.append('x');
