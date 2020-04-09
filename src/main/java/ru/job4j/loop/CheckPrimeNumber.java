d1973ff583d6e04624de19cc3e24670b7bc44a98
Mikhail Ford <mfordf150@mail.ru>
2020-04-09 09:26:04 +0300

    Fitness

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -5,15 +5,15 @@ public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime = true;
         if ((number % 5) == 0) {
-            return prime;
+        } else if ((number % 4) == 0) {
+            return false;
+        } else if ((number % 1) == 0) {
+            return false;
         }
+
 
         return prime;
     }
-    public static boolean check2(int number) {
-        boolean prime = false;
-        if((number % 4) == 0 );
-        return prime;
-    }
+
 }
 
--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -2,15 +2,9 @@ package ru.job4j.loop;
 
 public class Fitness {
     public static int calc(int ivan, int nik) {
-    if (ivan < nik) {
-
-    }
-    else {
         ivan = 90;
-        while (ivan < 100) {
-           ivan *=3;
-        }
-    }
+        nik = 95;
+        while (ivan < nik);
 
 
         int month = 0;
