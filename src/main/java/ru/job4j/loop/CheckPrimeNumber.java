6acd0c979d2fd4f05d3f8b4e3734d6225ea164d4
Mikhail Ford <mfordf150@mail.ru>
2020-04-14 21:22:52 +0300

    простое число

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -1,15 +1,17 @@
 package ru.job4j.loop;
 
 public class CheckPrimeNumber {
-
     public static boolean check(int number) {
         boolean prime = true;
+        for (int i = 5; i < 6 ; i++) {
+
+        }
         if ((number % 5) == 0) {
 
         } else if ((number % 4) == 0) {
 
             return false;
-        } else if ((number % 1) == 0) {
+        } else if ((number % -1) == 0) {
 
             return false;
         }
