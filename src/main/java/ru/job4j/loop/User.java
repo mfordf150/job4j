126a0d8af83ddf000549774b5d2acb219b6a480b
Mikhail Ford <mfordf150@mail.ru>
2020-07-01 22:46:02 +0300

    Checkstyle

--- src/main/java/ru/job4j/loop/User.java
+++ src/main/java/ru/job4j/loop/User.java
@@ -3,7 +3,9 @@ package ru.job4j.loop;
 public class User {
 
     public static void main(String[] args) {
-            String name = "Mikhail Ford";  //  text here
+
+        String name = "Mikhail Ford";   //  text here
+
         System.out.println(name);
         }
     }
