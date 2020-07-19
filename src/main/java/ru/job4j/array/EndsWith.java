2c3e2f62b6af42cdad564829cb4b311302044157
Mikhail Ford <mfordf150@mail.ru>
2020-07-19 17:19:56 +0300

    EndWIsh

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -3,19 +3,13 @@ package ru.job4j.array;
 public class EndsWith {
 
     public static boolean endsWith(char[] word, char[] post) {
-        boolean result = false;
-        for (int index = 0; index < word.length; index++) {
-
-            if (post[post.length - 1 - index] == word[word.length - 1 - index])  {
-                result = true;
-            } else{
+        boolean result = true;
+        for (int index = 0; index < post.length; index++) {
+            if (post[post.length - 1 - index] != word[word.length - 1 - index]) {
                 result = false;
                 break;
             }
         }
-
         return result;
-
     }
-
 }
