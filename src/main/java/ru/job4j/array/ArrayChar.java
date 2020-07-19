a21710b689f559f7dbb871783b403653a1638193
Mikhail Ford <mfordf150@mail.ru>
2020-07-19 10:59:15 +0300

    ArrayChar

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -4,8 +4,8 @@ public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
-        for (int index = 0; index <word.length; index++) {
-            if (word[0] != pref[index]) {
+        for (int index = 0; index <pref.length; index++) {
+            if (word[index] != pref[index]) {
                 result = false;
                 break;
             }
