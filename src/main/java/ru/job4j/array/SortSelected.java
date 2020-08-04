fbfbc567a0276b5ccf4eef55ca2711633b20d148
Mikhail Ford <mfordf150@mail.ru>
2020-08-04 20:21:53 +0300

    sort

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -7,24 +7,22 @@ public class SortSelected {
     public static int[] sort(int[] data) {
 
         for (int i = 0; i < data.length; i++) {
-            int min = MinDiapason.findMin(data, 0, data.length);
-            int index = FindLoop.indexOf(data, min, 0, data.length);
-            for (int j = i ; index < data.length ; j++) {
-                if(data[j] < min) {
-                 min = data[j];
-                }
 
-            }
-
-
-
-//            int min = indexOf(data, i + 1, i, data.length);
-//            int temp = data[min];
-//            data[min] = data[i];
-//            data[i] = temp;
+            int min = indexOf(data, i + 1, i, data.length);
+            int temp = data[min];
+            data[min] = data[i];
+            data[i] = temp;
 
         }
+
+
+
+
         return data;
+
+
     }
+
 }
+
 
