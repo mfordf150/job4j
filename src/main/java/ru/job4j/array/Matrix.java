a94f55e81692eec06652161e86f0f3ac573c1e7c
Mikhail Ford <mfordf150@mail.ru>
2020-08-09 00:36:58 +0300

    Matrix

---
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+public class Matrix {
+    public static int[][] multiple(int size) {
+
+
+        int table[][] = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                table[j][i] = (j + 1) * (i + 1);
+            }
+        }
+        return table;
+    }
+
+
+            }
+
+
