e22c45245f34a14bd625e990de128c36e9e0e723
Mikhail Ford <mfordf150@mail.ru>
2020-08-18 01:01:25 +0300

    Defragment

--- src/main/java/ru/job4j/array/Defragment.java
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -3,21 +3,25 @@ package ru.job4j.array;
 public class Defragment {
 
     public static String[] compress(String[] array) {
+        int index = 0;
+        String text = null;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null) {
+                text = array[i];
+                array[i] = null;
+                array[index] = text;
+                index = index + 1;
 
-        for (int index = array.length - 1; index >= 0; index--) {
-            for (int j = 0; j < index; j++) {
-                if (array[j] == null) {
-                    array[j] = array[j + 1];
-                    array[j + 1] = null;
 
-                }
-                int point = index; /* указатель на null ячейку. */
-                /* переместить первую не null ячейку. Нужен цикл. */
             }
-            System.out.print(array[index] + " ");
+
+
         }
         return array;
+
     }
+
+
 
     public static void main(String[] args) {
         String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
