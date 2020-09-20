f4419a5312ef26f984d364b607ffbf8edce7bed2
Mikhail Ford <mfordf150@mail.ru>
2020-09-20 19:34:39 +0300

    1.5. Котенок гав и его котлета [#176440]

--- chapter_002/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -5,24 +5,33 @@ public class Cat {
     private String food;
 
     public void giveNick(String nick) {
-
+        this.name = nick;
     }
-
     public void show() {
+        System.out.println(this.name);
         System.out.println(this.food);
+
+
     }
-    public  void eat(String meat) {
+
+    public void eat(String meat) {
         this.food = meat;
     }
+
     public static void main(String[] args) {
         System.out.println("There are gav's food");
         Cat gav = new Cat();
         gav.eat("kotleta");
+        gav.giveNick("kitty gav ate kotleta");
         gav.show();
         System.out.println("There are black's food");
         Cat black = new Cat();
         black.eat("fish");
+        black.giveNick("cat black ate fish");
         black.show();
 
+
     }
+
+
 }
