cd0ff69331411fbc23d11b1c02e728a7219030df
Mikhail Ford <mfordf150@mail.ru>
2020-09-25 15:27:00 +0300

    1.7. Магические сосуды.[167388#176445]

--- chapter_002/src/main/java/ru/job4j/oop/Battery.java
+++ chapter_002/src/main/java/ru/job4j/oop/Battery.java
@@ -1,19 +1,27 @@
 package ru.job4j.oop;
-
+//Создать класс ru.job4j.oop.Battery. в нем должно быть поле private int load.
+//Создать метод public void exchange(Battery another). Этот метод должен списывать заряд из батареи
+// у кого вызывали метод exchange и добавить к объекту another.
 public class Battery {
     private int load;
 
-    public int exchange(Battery another) {
-        this.load = this.load + another.load;
+    public Battery(int size) {
+        this.load = size;
+    }
 
+    public void exchange(Battery another) {
+        this.load = this.load + another.load;
         another.load = 0;
-        return 0;
+
     }
 
     public static void main(String[] args) {
-        Battery battery  = new Battery();
-        battery.exchange(battery);
-        System.out.println("battery charge  %" + battery.exchange(battery));
+        Battery battery = new Battery(50);
+        Battery battery2 = new Battery(10);
+        System.out.println("battery : " + battery.load + ". battery2 : " + battery2.load);
+        battery.exchange(battery2);
+        System.out.println("battery : " + battery.load + ". battery2 : " + battery2.load);
+
 
     }
 }
