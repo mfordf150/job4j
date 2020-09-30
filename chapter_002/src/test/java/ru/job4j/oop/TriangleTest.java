63454c0c0a64c4484d92cc31ae76ceff10a754b6
Mikhail Ford <mfordf150@mail.ru>
2020-10-01 00:05:46 +0300

    Рефакторинг - Расстояние между точками.[122587#176434]

--- chapter_002/src/test/java/ru/job4j/oop/TriangleTest.java
+++ chapter_002/src/test/java/ru/job4j/oop/TriangleTest.java
@@ -1,9 +1,9 @@
 package ru.job4j.oop;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.closeTo;
+
 
 public class TriangleTest {
         @Test
@@ -12,10 +12,9 @@ public class TriangleTest {
             Point b = new Point(4.0, 5.0);
             Point c = new Point(10.0, 8.0);
             Triangle tr = new Triangle(a, b, c);
-            double result = tr.area(2.0, 8.0, 4.0, 5.0, 10.0, 8.0);
-            assertThat(result, is(12.000000000000009));
-            String firstResult = String.format("%.3f", 12.000000000000009);
-            System.out.println((firstResult));
+            double result = tr.area(2.0, 8.0, 4.0, 5.0, 10.0, 8.0 );
+            closeTo(result, (12.009));
+
 
         }
 
