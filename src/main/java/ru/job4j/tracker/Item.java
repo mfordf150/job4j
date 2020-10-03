8e8fe19ad7a8c02f4b78863a5633f3a9c1eeb6ce
Mikhail Ford <mfordf150@mail.ru>
2020-10-03 15:36:37 +0300

    Перегрузка конструктора.[173890#176450]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -9,12 +9,23 @@ public class Item {
     }
 
     public Item(int id) {
+        id = 1;
 
     }
 
     public Item(int id, String name) {
-        
+        id = 2;
+        name = "Jhon";
+
     }
 
+    public static void main(String[] args) {
+        Item item = new Item();
+        Item item2 = new Item(1);
+        Item item3 = new Item(2, "Jhon");
+
+
+
+    }
 }
 
