f6a2edb1d96aaca2b5cd8213f37559cd6fbc0674
Mikhail Ford <mfordf150@mail.ru>
2020-10-03 14:26:40 +0300

    Перегрузка конструктора.[173890#176450]

---
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -0,0 +1,20 @@
+package ru.job4j.tracker;
+
+public class Item {
+    int id;
+    String name;
+
+    public Item() {
+
+    }
+
+    public Item(int id) {
+
+    }
+
+    public Item(int id, String name) {
+        
+    }
+
+}
+
