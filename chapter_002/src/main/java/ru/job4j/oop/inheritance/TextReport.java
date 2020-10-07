23f421702bd319a631365e3575f124351836049a
Mikhail Ford <mfordf150@mail.ru>
2020-10-07 23:49:52 +0300

    Переопределение[174890#176451] nothing here

--- chapter_002/src/main/java/ru/job4j/oop/inheritance/TextReport.java
+++ chapter_002/src/main/java/ru/job4j/oop/inheritance/TextReport.java
@@ -6,23 +6,19 @@ public class TextReport {
     }
 
     public class ReportUsage {
-        TextReport report = new TextReport();
-        String text = report.generate("Report's name", "Report's body");
-    }
-
-    public static void main(String[] args) {
-        TextReport report = new TextReport();
-        String text = report.generate("Report's name", "Report's body");
-        System.out.println(text);
+        public static void main(String[] args) {
+            TextReport report = new TextReport();
+            String text = report.generate("Report's name", "Report's body");
+            System.out.println(text);
+        }
     }
 
     public class HtmlReport extends TextReport {
         public String generate(String name, String body) {
-
             return "<h1>" + name + "</h1>" +
                     "<br/>" +
                     "<span>" + body + "</span>";
         }
     }
-
 }
+
