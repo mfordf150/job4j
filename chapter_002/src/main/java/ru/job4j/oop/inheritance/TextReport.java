d2f0bfc997549033a86669860400a0e315067eb1
Mikhail Ford <mfordf150@mail.ru>
2020-10-15 15:57:47 +0300

    Аннотация @Override[174891#176452]

--- chapter_002/src/main/java/ru/job4j/oop/inheritance/TextReport.java
+++ chapter_002/src/main/java/ru/job4j/oop/inheritance/TextReport.java
@@ -7,19 +7,31 @@ public class TextReport {
 
     public static class ReportUsage {
         public static void main(String[] args) {
-            JSONReport report = new  JSONReport();
-            String text = report.generate("name" + ",","" +  "body");
+            JSONReport report = new JSONReport();
+            HtmlReport report1 = new  HtmlReport();
+            String text = report.generate("name" + ",", "" + "body");
             System.out.println(text);
         }
     }
 
+    @Override
+    public static class HtmlReport extends TextReport {
+
+        public String genreate(String name, String body) {
+            return "<h1>" + name + "</h1>" +
+                    "<br/>" +
+                    "<span>" + body + "</span>";
+        }
+    }
+
+    @Override
     public static class JSONReport extends TextReport {
         public String generate(String name, String body) {
             return String.format("{\n name: %s \n body: %s \n}", name, body);
         }
     }
 
-    }
+}
 
 
 
